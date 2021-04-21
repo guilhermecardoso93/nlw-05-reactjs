1174585b41eab0fc76c49ec30907032a1e92ea51
Guilherme Cardoso da Silva <guilhermecardosowork@gmail.com>
2021-04-21 19:50:13 -0300

    firtscommit

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,43 @@
+import styles from './styles.module.scss';
+
+export function Player () {
+    return (
+      <div className={styles.playerContainer}>
+        <header>
+          <img src="/playing.svg" alt="Tocando agora"/>
+          <strong>Tocando Agora</strong>
+        </header>
+
+          <div className={styles.emptyPlayer}>
+            <strong>Selecione um podcast para ouvir</strong>
+          </div>
+          <footer className={styles.empty}>
+            <div className={styles.progress}>
+                <span>00:00</span>
+                <div className={styles.slider}>
+                  <div className={styles.emptySlider}/>
+                </div>
+                <span>00:00</span>
+            </div>
+
+              <div className={styles.buttons}>
+            <button type="button">
+              <img src="/shuffle.svg" alt="embaralhar"/>
+            </button>
+            <button type="button">
+              <img src="/play-previous.svg" alt="tocar anterior"/>
+            </button>
+            <button type="button" className={styles.playButton}>
+              <img src="/play.svg" alt="tocar"/>
+            </button>
+            <button type="button">
+              <img src="/play-next.svg" alt="tocar proxima"/>
+            </button>
+            <button type="button">
+              <img src="/repeat.svg" alt="repetir"/>
+            </button>
+          </div>
+            </footer>
+        </div>
+    );
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import {AppProps } from 'next/app';
+import '../styles/global.scss';
+
+import { Header } from '../components/Header';
+import {Player} from '../components/Player';
+
+import styles from '../styles/app.module.scss';
+
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header/>
+        <Component {...pageProps} />
+      </main>
+        <Player/>
+    </div>
+  )
+}
+
+export default MyApp;
